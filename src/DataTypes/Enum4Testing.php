ca4f8d09434e10db04a76b9c664079901e3475db
Patrick Nehls <pnehls@alphaonline.de>
2025-07-30 11:29:48 +0200

    feat: add enum values

--- src/DataTypes/Enum4Testing.php
+++ src/DataTypes/Enum4Testing.php
@@ -3,7 +3,7 @@
  * Copyright (c) 2022-2025 Iomywiab/PN, Hamburg, Germany. All rights reserved
  * File name: Enum4Testing.php
  * Project: Testing
- * Modified at: 21/07/2025, 10:18
+ * Modified at: 30/07/2025, 11:29
  * Modified by: pnehls
  */
 
@@ -14,4 +14,5 @@ namespace Iomywiab\Library\Testing\DataTypes;
 enum Enum4Testing
 {
     case ONE;
+    case TWO;
 }
--- src/DataTypes/IntEnum4Testing.php
+++ src/DataTypes/IntEnum4Testing.php
@@ -3,7 +3,7 @@
  * Copyright (c) 2022-2025 Iomywiab/PN, Hamburg, Germany. All rights reserved
  * File name: IntEnum4Testing.php
  * Project: Testing
- * Modified at: 21/07/2025, 10:18
+ * Modified at: 30/07/2025, 11:29
  * Modified by: pnehls
  */
 
@@ -14,4 +14,5 @@ namespace Iomywiab\Library\Testing\DataTypes;
 enum IntEnum4Testing: int
 {
     case ONE = 1;
+    case TWO = 2;
 }
